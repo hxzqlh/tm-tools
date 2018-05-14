089bed47b03827881c56a53b401e202cb2de5ced
hxzqlh <hxzqlh@gmail.com>
2018-05-14 17:41:18 +0800

    batch db

--- convert/blockstore.go
+++ convert/blockstore.go
@@ -1,10 +1,11 @@
 package convert
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/hxzqlh/tm-tools/util"
 	"github.com/tendermint/tendermint/types"
+	dbm "github.com/tendermint/tmlibs/db"
 )
 
 // simulate the BlockStore api of blockchain
@@ -23,32 +24,41 @@ func OnBlockStore(startHeight int) {
 		lastBlockID = &nMeta.BlockID
 	}
 
+	cnt := 0
+	limit := 1000
+	batch := nBlockDb.NewBatch()
 	for i := startHeight; i <= totalHeight; i++ {
-		fmt.Printf("convert height %v/%v\n", i, totalHeight)
+		cnt++
 
 		nBlock := NewBlockFromOld(i, lastBlockID)
 		blockParts := nBlock.MakePartSet(types.DefaultBlockPartSize)
 		nMeta := types.NewBlockMeta(nBlock, blockParts)
 		// seen this BlockId's commit
 		seenCommit := NewSeenCommit(i, &nMeta.BlockID)
-		SaveBlock(nBlock, nMeta, seenCommit)
+
+		SaveBlock(batch, nBlock, nMeta, seenCommit)
+		if cnt%limit == 0 {
+			log.Printf("batch write %v/%v\n", cnt, totalHeight)
+			batch.Write()
+			batch = nBlockDb.NewBatch()
+		}
 
 		// update lastBlockID
 		lastBlockID = &nMeta.BlockID
+	}
+	if cnt%limit != 0 {
+		log.Printf("batch write %v/%v\n", cnt, totalHeight)
+		batch.Write()
 	}
 
 	SaveState(lastBlockID)
 }
 
-func SaveBlock(block *types.Block, blockMeta *types.BlockMeta, seenCommit *types.Commit) {
+func SaveBlock(batch dbm.Batch, block *types.Block, blockMeta *types.BlockMeta, seenCommit *types.Commit) {
 	height := block.Height
-	batch := nBlockDb.NewBatch()
-
 	util.SaveNewBlockMeta2(batch, height, blockMeta)
 	util.SaveNewBlockParts2(batch, height, block)
 	util.SaveNewCommit2(batch, height-1, "C", block.LastCommit)
 	util.SaveNewCommit2(batch, height, "SC", seenCommit)
 	util.SaveNewBlockStoreStateJSON2(batch, height)
-
-	batch.Write()
 }
